e9ad248737ca64eee6a0b23ac21d03ef5ec39803
bassma218 <kharbach.bassma.solicode@gmail.com>
2024-11-21 09:24:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const name = document.getElementById('name');
+const yourjob = document.getElementById('job');
+const adrres = document.getElementById('info-section');
+const birthday = document.getElementById('birthday');
